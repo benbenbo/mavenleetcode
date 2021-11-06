80427d3ffa0205297900a03d2abaaa0563744d38
zhengbingyuan <zhengbingyuan@leyopharm.com>
2021-11-06 15:20:34 +0800

    feat(leetcode05): 动态规划法解题

--- src/test/java/com/zzb/test/LeetCode05.java
+++ src/test/java/com/zzb/test/LeetCode05.java
@@ -10,23 +10,32 @@ import org.junit.runners.JUnit4;
  **/
 @RunWith(JUnit4.class)
 public class LeetCode05 {
+    /**
+     * 测试方法
+     */
     @Test
-    public void testAll(){
-        String s = "babad";
-        String max = test01(s);
-        System.out.println("最大回文子串为："+max);
+    public void testAll() {
+        String s = "aba453aba";
+        String max = test02(s);
+        System.out.println("最大回文子串为：" + max);
     }
 
-    public String test01(String s){
-        int max=0;
+    /**
+     * 暴力解法-主程序
+     *
+     * @param s
+     * @return
+     */
+    public String test01(String s) {
+        int max = 0;
         String maxSub = "";
         //这里具有n平方的时间复杂度，isReverStr又有n的时间复杂度
         //空间复杂度为常数项
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i+1; j <= s.length(); j++) {
+            for (int j = i + 1; j <= s.length(); j++) {
                 String sub = s.substring(i, j);
-                if(isReverStr(sub)){
-                    if(sub.length()>max){
+                if (isReverStr(sub)) {
+                    if (sub.length() > max) {
                         maxSub = sub;
                         max = sub.length();
                     }
@@ -36,13 +45,53 @@ public class LeetCode05 {
         return maxSub;
     }
 
-    public boolean isReverStr(String s){
+    /**
+     * 暴力解法-判断是否为回文串
+     *
+     * @param s
+     * @return
+     */
+    public boolean isReverStr(String s) {
         int len = s.length();
-        for (int i = 0; i < len/2; i++) {
-            if(s.charAt(i)!=s.charAt(len-i-1)){
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
                 return false;
             }
         }
         return true;
+    }
+
+
+    /**
+     * 动态规划法解决
+     */
+    public String test02(String s) {
+        int len = s.length();
+        int maxLen = 0;
+        int maxEnd = 0;
+        String s2 = new StringBuilder(s).reverse().toString();
+        //动态规划数组
+        int[][] arr = new int[len][len];
+        //填充数组
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                if (s.charAt(i) == s2.charAt(j)) {
+                    if (i == 0 || j == 0) {
+                        arr[i][j] = 1;
+                    } else {
+                        arr[i][j] = arr[i - 1][j - 1] + 1;
+                    }
+                }
+                if (arr[i][j] > maxLen) {
+                    int i1 = len - 1 - i;
+                    int i2 = i1 + arr[i][j] - 1;
+                    if (i2 == j) {
+                        maxLen = arr[i][j];
+                        maxEnd = i;
+                    }
+                }
+            }
+        }
+        return s.substring(maxEnd - maxLen + 1, maxEnd + 1);
     }
 }
